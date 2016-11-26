b1af35d9170897e7750c3d1d71686b8f8207e71e
tn74 <tn74@duke.edu>
2016-11-26 14:41:28 -0500

    1) Creates a connectionless network 2) Can distinguish betweeen nodes that know and do not and display them graphically with diff colors

--- Test.R
+++ Test.R
@@ -1,28 +1,39 @@
 library(igraph)
 
 
-# First function called, should create the network
+# First function called, creates the nodes of the network
 createGraph = function(nodes = 10){
-  eg = make_empty_graph(nodes)
-  set_vertex_attr(eg, "name", 1, "Trishul Nagenalli")
-  plot(eg)
+  eg = make_empty_graph(nodes) 
+  eg = set_vertex_attr(eg, "knownState",V(eg), FALSE) 
+  eg = set_vertex_attr(eg, "color", V(eg), "red")
   return(eg)
 }
 
+# Create a number of edges stemming from a given node 
+createEdges = function(g, nodeIndex, numConnections){
+  numNodes = g.vcount()
+  if (numNodes<numConnections){
+    stop("More connections than nodes!")
+  }
+  toConnect = ceiling(runif(numConnections,0,numNodes))
+  g = add_edges() 
+}
+
 # Sets a node to be "known" and changes the fill color
-becomeKnown = function(g,nodeIndex){
-  print("Into becomeKnown")
-  set_vertex_attr(g,"known",nodeIndex,TRUE)
-  set_vertex_attr(g,"color", nodeIndex,"blue")
+becomeKnown = function(g, nodeIndex){
+  g = set_vertex_attr(g,"knownState",nodeIndex, TRUE) %>%
+    set_vertex_attr("color", nodeIndex,"green")
   return(g)
 }
 
 # Plots graphs as we wish
 plotGraph = function (g){
-  plot(g, edge.arrow.size=.5, vertex.label.color="black", vertex.label.dist=1.5,
-       vertex.color="pink" ) 
+  plot(g, edge.arrow.size=.5, vertex.label.color="black", vertex.label.dist=1)
 }
   
 
-graphObj = createGraph(50)
-plot(graphObj)
+graphObj = createGraph(100)
+graphObj = becomeKnown(graphObj,10)
+graphObj = createEdges(graphObj,10,5)
+plotGraph(graphObj)
+
