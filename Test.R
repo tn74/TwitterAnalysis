000e5e0baf7547de8dd17fc81b74a77cdce4755d
tn74 <tn74@duke.edu>
2016-12-04 16:36:21 -0500

    Got js network working

--- Test.R
+++ Test.R
@@ -1,4 +1,7 @@
 library(igraph)
+library(networkD3)
+library(dplyr)
+library(magrittr)
 
 
 # First function called, creates the nodes of the network
@@ -11,12 +14,17 @@ createGraph = function(nodes = 10){
 
 # Create a number of edges stemming from a given node 
 createEdges = function(g, nodeIndex, numConnections){
-  numNodes = g.vcount()
+  numNodes = length(V(g))
+  print(numNodes)
   if (numNodes<numConnections){
     stop("More connections than nodes!")
   }
-  toConnect = ceiling(runif(numConnections,0,numNodes))
-  g = add_edges() 
+  toConn = matrix(sample.int(numNodes, size = numConnections, replace = FALSE))
+  addConnMat = c(rep(nodeIndex,numConnections*2))
+  for (i in 1:numConnections){
+    addConnMat[2*i] = toConn[i]
+  }
+  g = add_edges(g, addConnMat ) 
 }
 
 # Sets a node to be "known" and changes the fill color
@@ -30,10 +38,17 @@ becomeKnown = function(g, nodeIndex){
 plotGraph = function (g){
   plot(g, edge.arrow.size=.5, vertex.label.color="black", vertex.label.dist=1)
 }
-  
 
-graphObj = createGraph(100)
+graphObj = createGraph(50)
 graphObj = becomeKnown(graphObj,10)
-graphObj = createEdges(graphObj,10,5)
-plotGraph(graphObj)
+for (i in 1:length(V(graphObj))){
+  graphObj = createEdges(graphObj,i,5)
+}
 
+
+wc <- cluster_walktrap(graphObj$known)
+members <- membership(wc)
+graphObj_d3 = igraph_to_networkD3(graphObj, group = members)
+forceNetwork(Links = graphObj_d3$links, Nodes = graphObj_d3$nodes,
+             Source = 'source', Target = 'target',
+             NodeID = 'name', Group = 'group')
